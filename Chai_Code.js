083884e693099e423a89f7517148334830446222
sheetalkkharat <sheetalkharatk@gmail.com>
2025-05-18 21:39:55 -0500

    Datatype conversion

---
+++ Chai_Code.js
@@ -0,0 +1,23 @@
+useName = "Sheetal"
+lastName = "kharat"
+console.log(useName);
+console.table([useName,lastName]);
+"use strict"; // treat all JS code as newer version
+// alert(3+3) we are using nodejs, not browser
+
+
+// DataTypes: number => 2 to power 53
+// bigint
+// string
+// Boolean
+// null=> standalone value
+// undefined 
+// symbol => unique
+// object
+
+let string1 = "Sam"
+let number1 = 1234
+let bool1 = true/false
+
+console.log(typeof string1);
+
---
+++ datatype-conversion.js
@@ -0,0 +1,25 @@
+let score = 33;
+console.log(typeof score);
+console.log(typeof(score));
+
+
+// String to number "abc" => NaN (Not a number)
+// Null will be converted to 0
+// Boolean gets converted to 1 or 0
+
+
+
+// Converting in boolean
+let isLoggedIn = "" // Empty string becomes false,1 =>true, 0=>false
+let booleanConv = Boolean(isLoggedIn)
+console.log(booleanConv);
+
+
+
+// operations
+
+console.log(1+ 2+"2"); //converts in number
+console.log("1" + 2 +2 ); // converts in string
+console.log(true + true); // boolean adding , never used though
+
+
